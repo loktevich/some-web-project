6bfc367410a6e9613866eb61a40070d0eec982a7
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 10:37:18 +0300

    creating a base interface for data access objects

---
+++ src/main/java/edu/shop/java/dao/ItemDAO.java
@@ -0,0 +1,20 @@
+package edu.shop.java.dao;
+
+import java.util.List;
+
+import edu.shop.java.model.Model;
+
+public interface ItemDAO<T extends Model> {
+
+    public List<T> getAll();
+
+    public T getById(Long id);
+
+    public void add(T model);
+
+    public void update(T model);
+
+    public void remove(Long id);
+
+    public void remove(T model);
+}
