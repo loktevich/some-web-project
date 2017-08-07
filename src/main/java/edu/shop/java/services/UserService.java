6d508cd3930b1e22db630d758d2acb92eef17e84
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 11:27:54 +0300

    creating user service

---
+++ src/main/java/edu/shop/java/services/UserService.java
@@ -0,0 +1,26 @@
+package edu.shop.java.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.shop.java.dao.UserDao;
+import edu.shop.java.model.User;
+
+public class UserService {
+    
+    @Autowired()
+    @Qualifier(value="userFileDao")
+    private UserDao userDao;
+    
+    public UserService() {
+        
+    }
+    
+    @Transactional
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
