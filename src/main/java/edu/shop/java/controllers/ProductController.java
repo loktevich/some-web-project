ae673fdb1e9f7c87cc7ae298010710ded7daee5c
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 21:27:48 +0300

    creating a product controller

---
+++ src/main/java/edu/shop/java/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package edu.shop.java.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    public ProductController() {
+
+    }
+
+}
