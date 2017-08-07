7b120e73414aa99bfca211db89c82fe226fbcecd
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 15:46:40 +0300

    adding a mapping to the user and creating many-to-many relation (users - roles)

--- src/main/java/edu/shop/java/model/User.java
+++ src/main/java/edu/shop/java/model/User.java
@@ -1,17 +1,50 @@
 package edu.shop.java.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "user")
-public class User extends Model {
+public class User extends Model implements UserDetails {
 
     private static final long serialVersionUID = -4490903198962576763L;
 
+    @Size(min = 5, max = 25)
+    @NotNull
+    @Column(name = "username", length = 25)
     private String username;
 
+    @NotNull
+    @Column(name = "password", length = 64)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id",
+                    nullable = false,
+                    updatable = false),
+            inverseJoinColumns = @JoinColumn(name = "role_id",
+                    nullable = false,
+                    updatable = false))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
         super();
@@ -19,6 +52,12 @@ public class User extends Model {
 
     public User(Long id) {
         super(id);
+    }
+
+    public User(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
     }
 
     public String getUsername() {
@@ -35,6 +74,47 @@ public class User extends Model {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> result = new ArrayList<>();
+        for (Role role : getRoles()) {
+            result.add(new SimpleGrantedAuthority(role.getTitle().name()));
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
