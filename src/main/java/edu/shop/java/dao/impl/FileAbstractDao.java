b30ddfcfdb2ac6045083c808b1c923e506ea0006
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 14:34:48 +0300

    refactoring

--- src/main/java/edu/shop/java/dao/impl/FileAbstractDao.java
+++ src/main/java/edu/shop/java/dao/impl/FileAbstractDao.java
@@ -4,20 +4,16 @@ import edu.shop.java.dao.accessors.FileDatasourceAccessor;
 import edu.shop.java.model.Model;
 
 public abstract class FileAbstractDao<T extends Model> extends FileDatasourceAccessor {
-    
+
     public void add(T model) {
-        
+
     }
-    
+
     public void update(T model) {
-        
+
     }
-    
-    public void remove(Long id) {
-        
-    }
-    
+
     public void remove(T model) {
-        this.remove(model.getId());
+
     }
 }
