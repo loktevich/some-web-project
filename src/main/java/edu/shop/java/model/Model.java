c0d8323a1e25e86c95763fc309d84c3cf367eb18
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 09:56:21 +0300

    creating a base class Model for all entities

---
+++ src/main/java/edu/shop/java/model/Model.java
@@ -0,0 +1,28 @@
+package edu.shop.java.model;
+
+import java.io.Serializable;
+
+public class Model implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -2921830326001375227L;
+    private Long id;
+
+    public Model() {
+
+    }
+
+    public Model(Long id) {
+        this.setId(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
