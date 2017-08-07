7d3c0ae641778422bf1bbcaf8c1bfce921f98993
Dmitry Loktevich <loktevich@gmail.com>
2017-08-07 14:55:24 +0300

    adding a mapping to the model (creating a primary key "id")

--- src/main/java/edu/shop/java/model/Model.java
+++ src/main/java/edu/shop/java/model/Model.java
@@ -2,10 +2,20 @@ package edu.shop.java.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Model implements Serializable {
 
     private static final long serialVersionUID = -2921830326001375227L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     public Model() {
