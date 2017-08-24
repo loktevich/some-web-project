7be707b1f0a5bdffa8080e7ffffc42aa89706a6e
Dmitry Loktevich <loktevich@gmail.com>
2017-08-24 23:12:20 +0300

    implementing a getByUsername method in the UserDatabaseDao class using Criteria

--- src/main/java/edu/shop/java/dao/impl/UserDatabaseDao.java
+++ src/main/java/edu/shop/java/dao/impl/UserDatabaseDao.java
@@ -1,15 +1,24 @@
 package edu.shop.java.dao.impl;
 
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import edu.shop.java.dao.UserDao;
 import edu.shop.java.model.User;
 
-@Repository
+@Repository("userDatabaseDao")
 public class UserDatabaseDao extends HibernateAbstractDao<User> implements UserDao {
 
     public UserDatabaseDao() {
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        Criteria criteria = getSession().createCriteria(User.class);
+        criteria.add(Restrictions.eq("username", username));
+        return (User) criteria.uniqueResult();
     }
 
 }
